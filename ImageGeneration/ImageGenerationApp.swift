6a9934757c53cbc52c444999a1e101ace2fabc42
rogelioCantu <rogeliofcj@gmail.com>
2023-05-04 20:48:09 -0700

    Initial Commit

---
+++ ImageGeneration/ImageGenerationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGenerationApp.swift
+//  ImageGeneration
+//
+//  Created by Rogelio Cantu on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ImageGenerationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
